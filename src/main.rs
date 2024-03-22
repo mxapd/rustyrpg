35b705633af5ab926862c551f766955d74f51d2b
Max <m.porseryd@gmail.com>
2024-03-22 02:35:09 +0100

    started working on game logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
+struct Player {
+    name: String,
+    background: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    main_menu();
+}
+
+fn main_menu() {
+    println!("1) New Game");
+    println!("2) Load Game");
+    println!("3) Quit");
+
+    new_game();
+}
+fn new_game() {
+    println!("What is your name?");
+
+    let mut player_name = String::new();
+
+    io::stdin()
+        .read_line(&mut player_name)
+        .expect("failed to get name");
 }
