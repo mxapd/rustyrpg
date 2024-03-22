ce5346987f3f11f47e0c3ab40e81756fb9d8c525
Max <m.porseryd@gmail.com>
2024-03-22 01:45:13 +0100

    created initial cargo files

---
+++ target/debug/deps/rustyrpg-46c2d0c9659d9dde.d
@@ -0,0 +1,5 @@
+/home/gucci/rustdev/rustyrpg/target/debug/deps/librustyrpg-46c2d0c9659d9dde.rmeta: src/main.rs
+
+/home/gucci/rustdev/rustyrpg/target/debug/deps/rustyrpg-46c2d0c9659d9dde.d: src/main.rs
+
+src/main.rs:
